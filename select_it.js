f4c137937c391162edbbd9b05df5534de4005263
Josh Hannan <joshh@aycmedia.com>
2014-09-30 16:27:39 -0400

    versioning script
    updated code to function more like typical select field

--- select_it.js
+++ select_it.js
@@ -23,8 +23,15 @@
 			$(this).after('<div id="select_'+count+'" class="select_it"><div class="select_it_box"><span class="displayed">'+placeholder+'</span><span class="tab"></span></div><ul style="display: none;">'+choices_array+'</ul></div><!--/select_it-->');
 			$('.select_it .select_it_box').click(function(e) {
 				e.stopPropagation();
-				current = $(this).parents('.select_it').attr('id');
-				$('#'+current+' ul').show();
+				if( $(this).hasClass('open') ) {
+					$(this).removeClass('open');
+					current = $(this).parents('.select_it').attr('id');
+					$('#'+current+' ul').hide();
+				} else {
+					$(this).addClass('open');
+					current = $(this).parents('.select_it').attr('id');
+					$('#'+current+' ul').show();
+				}
 			});
 			$('.select_it ul li').click(function(e) {
 				e.stopPropagation();
@@ -40,10 +47,12 @@
 						$(this).attr('selected', 'selected');
 					}
 				});
+				$('#'+current).children('.select_it_box').removeClass('open');
 				$('#'+current+' .select_it_box .displayed').html(selected);
 			});
 		});
 		$(document).click(function() {
+			$('.select_it .select_it_box').removeClass('open');
 			$('.select_it ul').hide();
 		});
 		return this;
