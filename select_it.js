28ea1fafbe7ae865287a1176711821f0a56a6f42
Josh Hannan <joshh@aycmedia.com>
2014-10-01 15:50:38 -0400

    Multiple Instances Update
    added functionality that is more compatible with current select drop
    down functionalities.  Can now click on displayed to close tab as well
    as away from the list to reset drop down.  Resolved issue with multiple
    instances after implementing new feature.

--- select_it.js
+++ select_it.js
@@ -22,30 +22,32 @@
 			choices_array = choices_array.join('\r');
 			$(this).after('<div id="select_'+count+'" class="select_it"><div class="select_it_box"><span class="displayed">'+placeholder+'</span><span class="tab"></span></div><ul style="display: none;">'+choices_array+'</ul></div><!--/select_it-->');
 			$('.select_it .select_it_box').click(function(e) {
-				e.stopPropagation();
-				if( $(this).hasClass('open') ) {
-					$(this).removeClass('open');
-					current = $(this).parents('.select_it').attr('id');
-					$('#'+current+' ul').hide();
+				e.stopImmediatePropagation();
+				current = $(this).parents('.select_it').attr('id');
+				console.log(current);
+				if( $('#'+current+' .select_it_box').hasClass('open') ) {
+					$('.select_it_box').removeClass('open');
+					$('.select_it ul').hide();
 				} else {
-					$(this).addClass('open');
-					current = $(this).parents('.select_it').attr('id');
+					$('.select_it_box').removeClass('open');
+					$('.select_it ul').hide();
+					$('#'+current+' .select_it_box').addClass('open');
 					$('#'+current+' ul').show();
 				}
 			});
 			$('.select_it ul li').click(function(e) {
-				e.stopPropagation();
+				e.stopImmediatePropagation();
 				current = $(this).parents('.select_it').attr('id');
 				$('#'+current+' ul li').removeClass('selected');
 				$('#'+current+' ul').hide();
 				selected = $(this).attr('rel');
 				selected_text = $(this).html();
 				$(this).addClass('selected');
-				$(this).parents('.select_it').prev('select').find('option').each(function() {
+				$(this).parents('#'+current).prev('select').find('option').each(function() {
 					select_check = $(this).val();
+					console.log(select_check);
 					if( select_check == selected ) {
-						console.log(select_check);
-						$('select').find('option').removeAttr('selected');
+						$(this).parents('select').find('option').removeAttr('selected');
 						$(this).attr('selected', 'selected');
 					}
 				});
