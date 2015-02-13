a1cdfe41b98b487bb7d684ee2729f95028dcadd6
Josh Hannan <joshh@aycmedia.com>
2015-02-13 15:45:36 -0500

    added tab indexing to plugin
    not finalized but workable for current usage.  Note:  need to
    modularize further on lines 90-93

--- select_it.js
+++ select_it.js
@@ -2,20 +2,28 @@
 	$.fn.select_it = function( options ) {
 		var choices_array = [];
 		var count = 0;
+		var key_check;
 		$(this).each(function() {
+			if( $(this).parent('form') ) {
+				var element_count = $(this).parent('form').children().length;
+				console.log(element_count);
+				element_count = '';
+			}
 			choices_array = [];
 			count++;
-			var placeholder, selected, select_check, current, onchange;
+			var placeholder, selected, select_check, current, onchange, tabindex;
 			var settings = $.extend({
 				onchange: 'none',
 				onchange_url:  'gallery_ajax.php',
 				onchange_container: '.gallery .carousel.block',
 				returned_div: 'toggled',
-				placeholder: '...'
+				placeholder: '...',
+				tabindex: 0
 			}, options );
 			var option_count = 0;
 			$(this).hide();
 			var choices = $(this).html();
+			if( $(this).attr('tabindex') ) { tabindex = $(this).attr('tabindex'); } else { tabindex = settings.tabindex; }
 			if( $(this).data('placeholder') ) { placeholder = $(this).data('placeholder'); } else { placeholder = settings.placeholder; }
 			$(this).find('option').each(function() { option_count++; if( option_count != 1 && $(this).is(':selected') ) { placeholder = $(this).text(); } });
 			if( $(this).data('onchange') ) { onchange = $(this).data('onchange'); } else { onchange = settings.onchange; }
@@ -25,7 +33,7 @@
 				choices_array[index] = ['<li rel="'+choice_value+'">'+choice_text+'</li>'];
 			});
 			choices_array = choices_array.join('\r');
-			$(this).after('<div id="select_'+count+'" class="select_it"><div class="select_it_box"><span class="displayed">'+placeholder+'</span><span class="tab"></span></div><ul style="display: none;">'+choices_array+'</ul></div><!--/select_it-->');
+			$(this).after('<div id="select_'+count+'" class="select_it"><div class="select_it_box" tabindex="'+tabindex+'"><span class="displayed">'+placeholder+'</span><span class="tab"></span></div><ul style="display: none;">'+choices_array+'</ul></div><!--/select_it-->');
 			$('.select_it .select_it_box').click(function(e) {
 				e.stopImmediatePropagation();
 				current = $(this).parents('.select_it').attr('id');
@@ -38,6 +46,51 @@
 					$('#'+current+' .select_it_box').addClass('open');
 					$('#'+current+' ul').show();
 				}
+			});
+			$('.select_it .select_it_box').keydown(function(e) {
+				e.stopImmediatePropagation();
+				current = $(this).parents('.select_it').attr('id');
+				if( e.keyCode == 40 ) {
+					$('#'+current+' .select_it_box').addClass('open');
+					//$('#'+current+' ul').show();
+					key_check = 0;
+					$('.select_it ul li').each(function() {
+						if( $(this).hasClass('selected') ) { key_check++; }
+					});
+					if( key_check === 0 ) {
+						$('#'+current+' ul li:first-child').addClass('selected');
+					} else {
+						$('#'+current+' ul li.selected').next().addClass('selected');
+						$('#'+current+' ul li.selected').first().removeClass('selected');
+						selected = $('#'+current+' ul li.selected').html();
+						$('#'+current+' .select_it_box .displayed').html(selected);
+					}
+				}
+				if( e.keyCode == 38 ) {
+					key_check = 0;
+					$('.select_it ul li').each(function() {
+						if( $(this).hasClass('selected') ) { key_check++; }
+					});
+					if( key_check === 0 ) {
+						$('#'+current+' ul li:first-child').addClass('selected');
+					} else {
+						$('#'+current+' ul li.selected').prev().addClass('selected');
+						$('#'+current+' ul li.selected').last().removeClass('selected');
+						selected = $('#'+current+' ul li.selected').html();
+						$('#'+current+' .select_it_box .displayed').html(selected);
+					}
+				}
+				current = $(this).parents('.select_it').attr('id');
+				if( e.keyCode == 13 ) {
+					$('.select_it_box').removeClass('open');
+					$('.select_it ul').hide();
+					selected = $('#'+current+' ul li.selected').html();
+					$('#'+current+' .select_it_box .displayed').html(selected);
+				}
+				if( e.keyCode == 9 ) {
+					$('#'+current).parents('form > .fieldset .block').next().find('input, textarea').focus();
+				}
+				return false;
 			});
 			$('.select_it ul li').click(function(e) {
 				e.stopImmediatePropagation();
@@ -63,7 +116,7 @@
 					$('#'+toggled).show(200);
 				}
 				if( onchange == 'load' ) {
-					$(settings.onchange_container).html('<img class="loading" src="http://localhost/~webdesigner/cfa_site/wp-content/themes/cfa_theme/images/loading.gif" />');
+					$(settings.onchange_container).html('<img class="loading" src="images/loading.gif" />');
 					var value = $(this).attr('rel');
 					console.log(value);
 					$.ajax({
